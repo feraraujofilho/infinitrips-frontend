9ba713ae2a89a342a7f02a737db1368da80c0c51
Fernando Araujo <fernando.araujo.filho@tenderframe.de>
2020-07-27 19:23:38 +0200

    fix cities

--- src/component/flightsTable/FlightsTableStyles.ts
+++ src/component/flightsTable/FlightsTableStyles.ts
@@ -14,7 +14,7 @@ export default makeStyles({
         maxHeight: "100vh",
         overflow: "auto",
         '@media only screen and (max-width: 768px)': {
-            width: "100%",
+            width: "100vw",
         },
         margin: "auto",
         boxShadow: "0px 6px 6px -3px rgba(0,0,0,0.2), 0px 10px 14px 1px rgba(0,0,0,0.14), 0px 4px 18px 3px rgba(0,0,0,0.12)"
