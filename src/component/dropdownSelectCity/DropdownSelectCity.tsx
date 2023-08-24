223cccd5f06dd130f48efb2fb1932d3bfddfa2aa
feraraujofilho <fernando@codeby.com.br>
2023-08-24 11:37:44 +0200

    fixed sorting

--- src/component/dropdownSelectCity/DropdownSelectCity.tsx
+++ src/component/dropdownSelectCity/DropdownSelectCity.tsx
@@ -1,37 +1,48 @@
-import React, { FC, ReactElement } from 'react';
-import { useQuery } from '@apollo/client';
-import citiesQueries from './apollo/citiesQueries';
-import { TextField, MenuItem } from '@material-ui/core';
-import { City } from './interfaces/City';
-import DropdownSelectCityProps from './DropdownSelectCityProps';
-import useStyles from './DropdownSelectCityStyles';
-import LoadingSpinner from "../loadingSpinner/loadingSpinner"
+import React, { FC, ReactElement } from 'react'
+import { useQuery } from '@apollo/client'
+import citiesQueries from './apollo/citiesQueries'
+import { TextField, MenuItem } from '@material-ui/core'
+import { City } from './interfaces/City'
+import DropdownSelectCityProps from './DropdownSelectCityProps'
+import useStyles from './DropdownSelectCityStyles'
+import LoadingSpinner from '../loadingSpinner/loadingSpinner'
 
-const DropdownSelectCity: FC<DropdownSelectCityProps> = ({ label, handleInputChange, name, value }) => {
-	const classes = useStyles();
+const DropdownSelectCity: FC<DropdownSelectCityProps> = ({
+	label,
+	handleInputChange,
+	name,
+	value,
+}) => {
+	const classes = useStyles()
 
-	const { data, loading } = useQuery(citiesQueries.Cities);
+	const { data, loading } = useQuery(citiesQueries.Cities)
 
 	// const destinationCities = ["London (alle)","Mailand (alle)", "Barcelona (BCN)", "Madrid (MAD)","Rom (alle)", "Porto (OPO)", "Dublin (DUB)",  "Berlin (alle)", "Palma de Mallorca (PMI)", "Edinburgh (EDI)", "Ibiza (IBZ)", "Venedig (alle)", "Nizza (NCE)"]
 
-	const originOptions = [<MenuItem value={'Berlin (alle)'} key={'Berlin (alle)'}> Berlin (alle) </MenuItem>,
-	<MenuItem value={'London (alle)'} key={'London (alle)'}> London (alle) </MenuItem>]
+	const originOptions = [
+		<MenuItem value={'Berlin'} key={'Berlin'}>
+			{' '}
+			Berlin{' '}
+		</MenuItem>,
+		<MenuItem value={'London (alle)'} key={'London (alle)'}>
+			{' '}
+			London (alle){' '}
+		</MenuItem>,
+	]
 
 	const renderMenuItems = (): ReactElement[] => {
-		if (name === "origin") {
+		if (name === 'origin') {
 			return originOptions
-		}
-		else {
+		} else {
 			return data?.cities.map((city: City) => {
 				return (
 					<MenuItem value={city.name} key={city.name}>
 						{city.name}
 					</MenuItem>
-				);
+				)
 			})
 		}
 	}
-
 
 	return (
 		<TextField
@@ -50,7 +61,7 @@ const DropdownSelectCity: FC<DropdownSelectCityProps> = ({ label, handleInputCha
 			</MenuItem>
 			{loading ? <LoadingSpinner /> : renderMenuItems()}
 		</TextField>
-	);
-};
+	)
+}
 
-export default DropdownSelectCity;
+export default DropdownSelectCity
--- src/domain/content/searchResults/SearchResults.tsx
+++ src/domain/content/searchResults/SearchResults.tsx
@@ -1,15 +1,14 @@
-import React, { FC, useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import FlightsTable from '../../../component/flightsTable/FlightsTable';
-import { get, isNumber, isString } from 'lodash';
-import SearchBox from '../../../component/searchBox/SearchBox';
-import useUrlSearchParams from '../../../app/hooks/useUrlSearchParams';
-import SortingMenu from '../../../component/sortingMenu/SortingMenu';
-import useStyles from "./SearchResultsStyles"
-import ExplanationColors from '../../../component/explanationColors/ExplanationColors';
-import axios from 'axios';
-import { Row } from '../../../app/types/Row';
-
+import React, { FC, useState, useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+import FlightsTable from '../../../component/flightsTable/FlightsTable'
+import { get, isNumber, isString } from 'lodash'
+import SearchBox from '../../../component/searchBox/SearchBox'
+import useUrlSearchParams from '../../../app/hooks/useUrlSearchParams'
+import SortingMenu from '../../../component/sortingMenu/SortingMenu'
+import useStyles from './SearchResultsStyles'
+import ExplanationColors from '../../../component/explanationColors/ExplanationColors'
+import axios from 'axios'
+import { Row } from '../../../app/types/Row'
 
 const SearchResults: FC = () => {
 	const [weekdaysFilter, setWeekdaysFilter] = useState<string[]>([])
@@ -19,27 +18,26 @@ const SearchResults: FC = () => {
 
 	const classes = useStyles()
 
-	const location = useLocation();
-	const { state } = location;
-	const query = useUrlSearchParams();
+	const location = useLocation()
+	const { state } = location
+	const query = useUrlSearchParams()
 
-	const origin = query.get('origin');
-	const destination1 = query.get('destination1');
-	const destination2 = query.get('destination2');
-	const destination3 = query.get('destination3');
-	const destination4 = query.get('destination4');
-	const nights = query.get('nights');
+	const origin = query.get('origin')
+	const destination1 = query.get('destination1')
+	const destination2 = query.get('destination2')
+	const destination3 = query.get('destination3')
+	const destination4 = query.get('destination4')
+	const nights = query.get('nights')
 
 	const destinationsObject = {
 		destination1: destination1,
 		destination2: destination2,
 		destination3: destination3,
-		destination4: destination4
-	};
+		destination4: destination4,
+	}
 
-	const tableData = get(state, 'data', null);
+	const tableData = get(state, 'data', null)
 	const filters = get(state, 'filters', [])
-
 
 	useEffect(() => {
 		if (filters.length > 0) {
@@ -47,12 +45,10 @@ const SearchResults: FC = () => {
 		}
 	}, [filters])
 
-
 	useEffect(() => {
 		if (tableData) {
 			setDynamicTableData(tableData)
-		}
-		else {
+		} else {
 			axios
 				.post(`${process.env.REACT_APP_BACKEND_URL}/flights`, {
 					origin: origin,
@@ -60,38 +56,48 @@ const SearchResults: FC = () => {
 					destination2: destination2,
 					destination3: destination3,
 					destination4: destination4,
-					nights: nights
+					nights: nights,
 				})
 				.then((response) => {
 					let dataForFrontend = response.data
 					if (weekdaysFilter.length > 0) {
-						const dataFilteredByWeekDays = dataForFrontend.filter((val: Row) => {
-							let valueArray = val.departuredate.split(" - ")
-							if (weekdaysFilter.indexOf(valueArray[0]) > -1) {
-								return val
+						const dataFilteredByWeekDays = dataForFrontend.filter(
+							(val: Row) => {
+								let valueArray = val.departuredate.split(' - ')
+								if (weekdaysFilter.indexOf(valueArray[0]) > -1) {
+									return val
+								}
 							}
-						})
+						)
 						dataForFrontend = dataFilteredByWeekDays
 					}
 					setDynamicTableData(dataForFrontend)
 				})
-				.catch((err) => console.log(err));
+				.catch((err) => console.log(err))
 		}
 	}, [tableData])
 
 	const handleSorting = (element: number | string) => {
 		if (isNumber(element)) {
 			let sortedData = tableData.sort((a: any, b: any) => {
-				if (a[element] && b[element]) {
-					return a[element] - b[element]
+				const aValue = a[element]
+				const bValue = b[element]
+
+				if (aValue === undefined || bValue === undefined) {
+					// Handle cases where one or both values are undefined
+					return aValue === undefined ? 1 : -1 // Put undefined values at the end
 				}
+
+				// Handle cases where both values are defined
+				return aValue - bValue
 			})
+
 			setDynamicTableData(sortedData)
 		}
 		if (isString(element)) {
 			let sortedDataByDate = tableData.sort((a: any, b: any) => {
-				let dateA = a.departuredate.split(" - ")[1]
-				let dateB = b.departuredate.split(" - ")[1]
+				let dateA = a.departuredate.split(' - ')[1]
+				let dateB = b.departuredate.split(' - ')[1]
 				return new Date(dateA).getTime() - new Date(dateB).getTime()
 			})
 			setDynamicTableData(sortedDataByDate)
@@ -99,19 +105,32 @@ const SearchResults: FC = () => {
 		setRerender(!rerender)
 	}
 
-
 	return (
 		<div className={classes.root}>
-			<SearchBox searchInfo={{ origin: origin, nights, ...destinationsObject }} weekdaysFilter={weekdaysFilter} setWeekdaysFilter={setWeekdaysFilter} setResetSorting={setResetSorting} />
+			<SearchBox
+				searchInfo={{ origin: origin, nights, ...destinationsObject }}
+				weekdaysFilter={weekdaysFilter}
+				setWeekdaysFilter={setWeekdaysFilter}
+				setResetSorting={setResetSorting}
+			/>
 			<div>
 				<div className={classes.sortingAndExplanation}>
-					<SortingMenu className={classes.sortingMenu} destinations={destinationsObject} handleSorting={handleSorting} resetSorting={resetSorting} setResetSorting={setResetSorting} />
+					<SortingMenu
+						className={classes.sortingMenu}
+						destinations={destinationsObject}
+						handleSorting={handleSorting}
+						resetSorting={resetSorting}
+						setResetSorting={setResetSorting}
+					/>
 					<ExplanationColors />
 				</div>
-				<FlightsTable data={dynamicTableData} destinations={destinationsObject} />
+				<FlightsTable
+					data={dynamicTableData}
+					destinations={destinationsObject}
+				/>
 			</div>
 		</div>
-	);
-};
+	)
+}
 
-export default SearchResults;
+export default SearchResults
